c30711b3832da91a3636dc26ee76b0bb1b18fb34
Juan Camilo Salazar Restrepo <juancamilosalazarrestrepo@gmail.com>
2025-11-10 21:28:50 -0500

    implemented vercer analitics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next";
 
 export const metadata: Metadata = {
   title: "Nice Gradient - Beautiful CSS Gradients Collection",
@@ -25,6 +26,7 @@ export default function RootLayout({
     <html lang="en">
       <body className="antialiased">
         {children}
+        <Analytics />
       </body>
     </html>
   );
